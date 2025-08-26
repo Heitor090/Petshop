74b955b33ca2ea672cf14ad49045f14e1355269d
heitor.bleao <heitor.bleao@TIT0676444W11-1>
2025-08-25 22:21:39 -0300

    Adicionar arquivos de projeto.

---
+++ Petshop/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petshop_Console
+{
+    public class Animal
+    {
+        public string nome;
+        private string especie {get; set;}
+
+        public double idade;
+
+     
+
+
+        public virtual void Comportamento()
+        {
+            Console.WriteLine($"Sem comportamento");
+
+        }
+
+       
+
+        public virtual void ValidarIdade()
+        {
+
+            if (idade <= 0)
+            {
+                Console.WriteLine("Idade não válida");
+            }
+        }
+    }
+}
---
+++ Petshop/Program.cs
@@ -0,0 +1,116 @@
+﻿using Petshop_Console;
+using System;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+
+public class Program
+{
+
+    public static void Main()
+    {
+        int escolha; 
+       
+           
+        do
+        {
+            Console.WriteLine("Escolha a informação desejada");
+            Console.WriteLine("1 - Cadastrar animal");
+            Console.WriteLine("2 - Mostrar lista");
+            Console.WriteLine("3 - Atendimento");
+            Console.WriteLine("4 - Sair");
+            escolha = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Animal[] atendimento = new Animal[2];
+            if (escolha == 1) {
+               
+               
+                    for (int i = 0; i < atendimento.Length; i++)
+                    {
+                        atendimento[i] = new Animal();
+                        Console.WriteLine("Escreva a espécie do seu petizin: ");
+                        atendimento[i].especie = Convert.ToString(Console.ReadLine());
+
+                        Console.WriteLine("Escreva o nome do seu Aniemal: ");
+                        atendimento[i].nome = Convert.ToString(Console.ReadLine());
+
+                        Console.WriteLine("Escreva a velhice do seu bischço: ");
+                        atendimento[i].idade = Convert.ToDouble(Console.ReadLine());
+                        Console.Clear();
+
+                        
+
+                                   }
+                
+               
+                
+            }
+                Console.WriteLine("Escolha a informação desejada");
+                Console.WriteLine("1 - Cadastrar animal");
+                Console.WriteLine("2 - Mostrar lista");
+                Console.WriteLine("3 - Atendimento");
+                Console.WriteLine("4 - Sair");
+                escolha = Convert.ToInt32(Console.ReadLine());
+                
+
+             if (escolha == 2)
+            {
+                for (int i = 0; i < atendimento.Length; i++)
+                {
+                   Console.WriteLine($"{i} - Especie: {atendimento[i].especie} | Nome: {atendimento[i].nome} | Idade: {atendimento[i].idade}");
+
+                }
+            }
+
+            Console.WriteLine("Escolha a informação desejada");
+            Console.WriteLine("1 - Cadastrar animal");
+            Console.WriteLine("2 - Mostrar lista");
+            Console.WriteLine("3 - Atendimento");
+            Console.WriteLine("4 - Sair");
+            escolha = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (escolha == 3)
+            {
+                Console.WriteLine("Indice iniciando");
+                Console.Clear();
+                if (escolha == 2)
+                {
+                    for (int i = 0; i < atendimento.Length; i++)
+                    {
+                        Console.WriteLine($"{i} - Especie: {atendimento[i].especie} | Nome: {atendimento[i].nome} | Idade: {atendimento[i].idade}");
+
+                    }
+                }
+                Console.WriteLine("Escolha indice desejado 0 ou 1");
+                int indice = Convert.ToInt16(Console.ReadLine());
+
+                ////if (indice == 0)
+                //{
+                //    atendimento[0].especie
+                //}
+                //else if (indice == 1)
+                //{
+                //    atendimento[1].Comportamento();
+                //}
+            }
+            Console.WriteLine("Escolha a informação desejada");
+            Console.WriteLine("1 - Cadastrar animal");
+            Console.WriteLine("2 - Mostrar lista");
+            Console.WriteLine("3 - Atendimento");
+            Console.WriteLine("4 - Sair");
+            escolha = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        } while (escolha > 4);
+
+    }
+
+
+
+
+}
+
