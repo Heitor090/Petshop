bc162fe999e9a948685ae6b165a6a316d9bd402b
Heitor090 <heitor.bleao@senacsp.edu.br>
2025-08-25 22:24:13 -0300

    Add files via upload

---
+++ Cachorro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petshop_Console
+{
+    public class Cachorro : Animal
+    {
+        public 
+        public override void Comportamento()
+        {
+
+            Console.WriteLine($"O {nome} acabou de latir");
+        }
+
+
+    }
+}
---
+++ Gato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petshop_Console
+{
+    public class Gato : Animal
+    {
+        public override void Comportamento()
+        {
+            Console.WriteLine($"O {nome} acabou de miar");
+        }
+
+           }
+}
